f4ce60a06e55f0b7bffca3e99f87db9ef004cc68
Vicente <vicentejiri@gmail.com>
2022-05-22 16:29:47 +0200

    primer commit

---
+++ inicio.java
@@ -0,0 +1,10 @@
+public class inicio
+{
+     public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
