d7ae5911dfa8fe737cb3c42a462194a3df44b37f
practicasformador <106074596+practicasformador@users.noreply.github.com>
2022-05-23 13:09:09 +0200

    Update form.java
    Añadimos sentencia switch al archivo form.java

---
+++ form.java
@@ -0,0 +1,19 @@
+// declaración de switch
+switch(expresión)
+{
+   // declaración case
+   // los valores deben ser del mismo tipo de la expresión
+   case valor1 :
+      // Declaraciones
+      break; // break es opcional
+   
+   case valor2 :
+      // Declaraciones
+      break; // break es opcional
+   
+   // Podemos tener cualquier número de declaraciones de casos o case
+   // debajo se encuentra la declaración predeterminada, que se usa cuando ninguno de los casos es verdadero.
+   // No se necesita descanso en el case default
+   default : 
+      // Declaraciones
+}
